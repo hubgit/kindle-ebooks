1b7fbc90f7a121dd806055335d7b35a5a45eb055
Alf Eaton <alf@hubmed.org>
2013-07-16 11:03:03 +0100

    Change page title

--- search.html.php
+++ search.html.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <meta charset="utf-8">
-<title>Search Kindle eBooks</title>
+<title>Top-Rated Kindle eBooks</title>
 <link rel="stylesheet" href="books.css">
 
 <? foreach ($items as $item) require __DIR__ . '/book.html.php'; ?>
