504f159cd68aa131b88ed5b271cafb4703ab04de
Alf Eaton <alf@hubmed.org>
2013-07-17 13:26:07 +0100

    add fork link

--- description.html.php
+++ description.html.php
@@ -15,3 +15,6 @@
 
 	<button type="submit">Search</button>
 </form>
+
+<a href="https://github.com/hubgit/kindle-ebooks/"><img style="position: absolute; top: 0; right: 0; border: 0;"
+	src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png" alt="Fork me on GitHub"></a>
